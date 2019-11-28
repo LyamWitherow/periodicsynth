c92541c207b9338b1775b0f621eb922d9b182527
Lyam Witherow <lyamcwitherow@gmail.com>
2019-11-28 11:25:50 -0600

    Fixed typos and compile errors.

--- README.md
+++ README.md
@@ -1,7 +1,7 @@
 # PeriodicSynth
 [![Build Status](https://travis-ci.org/nullvideo/periodicsynth.svg?branch=master)](https://travis-ci.org/nullvideo/periodicsynth)
 
-A basic periodic waveform synthesizer for generating the most common types of waveforms that a signal generator is able to generate. Basically, it mimics the capabalities of a signal generator.
+A basic periodic waveform synthesizer for generating the most common types of waveforms that a signal generator is able to generate. Basically, it mimics the capabilities of a signal generator.
 
 ---
 
@@ -13,21 +13,21 @@ periodicsynth = "0.1.2"
 ```rust
 /**
  * A basic usage of the library, a triangle wave
- * with a frequency of 440hZ and samplerate of
+ * with a frequency of 440hZ and sample rate of
  * 44.1khZ and bit-depth of 64bits.
  */
 use periodicsynth;
 
 fn main() {
-    /* Intialise an vector with 0 values. 
-       The length of the vector is the samplerate. */
+    /* Intialise an vector with 0 values.
+       The length of the vector is the sample rate. */
     let mut samples = vec![0f64; 44100];
 
     /* Synthesize a triangle wave with frequency of 440hZ. */
-    periodicsynth::synth(&samples, periodicsynth::PereodicFunction::Traingle, 440.0);
+    periodicsynth::synth(&mut samples, periodicsynth::PerodicFunction::Sine, 440.0);
 }
 ```
 
 
 ## Motivation
-The WebAudio API's [`OscillatorNode`](https://developer.mozilla.org/en-US/docs/Web/API/OscillatorNode) generates pereodic waveforms on demand with several types of pereodic function types with arbitary frequency and samplerate.
+The WebAudio API's [`OscillatorNode`](https://developer.mozilla.org/en-US/docs/Web/API/OscillatorNode) generates periodic waveforms on demand with several types of periodic function types with arbitrary frequency and sample rate.
--- src/lib.rs
+++ src/lib.rs
@@ -1,9 +1,9 @@
 use std::f64::consts::PI;
 
 /// List of periodic functions can be synthesised,
-/// also includes a Null synthesizer for generating 
+/// also includes a Null synthesizer for generating
 /// signals with `0` values.
-pub enum PerodicFunction {
+pub enum PeriodicFunction {
     Sine,
     Square,
     Triangle,
@@ -14,49 +14,49 @@ pub enum PerodicFunction {
 
 /// Synthesizes a periodic waveform with a arbitary frequency and samplerate.
 /// Note that, frequency should be less than half the samplerate (as of the nyquist theorem).
-/// 
+///
 /// #### Functions implemented:
-/// - Sine
-/// - Square
-/// - Triangle
-/// - Sawtooth
-/// 
+/// - sine
+/// - square
+/// - triangle
+/// - sawtooth
+///
 /// **Note:** the size of the output vector is considered the samplerate,
 /// the output vector must be resized before synthesizing.
-/// 
+///
 /// ## Arguments
 /// * `samples` &mdash; Vector to render oscillated samples into.
 /// * `pfunc` &mdash; Periodic function used to oscillate.
 /// * `freq` &mdash; Frequency of the function to oscillate on.
-pub fn synth(samples: &mut Vec<f64>, pfunc: PerodicFunction, freq: f64) {
+pub fn synth(samples: &mut Vec<f64>, pfunc: PeriodicFunction, freq: f64) {
     let sample_rate = samples.len();
-    
+
     match pfunc {
-        Sine => {
+        PeriodicFunction::Sine => {
             for (i, s) in samples.iter_mut().enumerate() {
-                let p = i as f64 / sample_rate as f64; 
+                let p = i as f64 / sample_rate as f64;
                 *s = ((2.0 * PI * freq) / p).sin();
             }
         },
-        Square => {
+        PeriodicFunction::Square => {
             for (i, s) in samples.iter_mut().enumerate() {
-                let p = i as f64 / sample_rate as f64; 
+                let p = i as f64 / sample_rate as f64;
                 *s = ((2.0 * PI * freq) / p).sin().signum();
             }
         },
-        Triangle => {
+        PeriodicFunction::Triangle => {
             for (i, s) in samples.iter_mut().enumerate() {
-                let p = i as f64 / sample_rate as f64; 
+                let p = i as f64 / sample_rate as f64;
                 *s = 4.0/p * (freq - p/2.0 * (2.0*freq / p + 0.5).floor());
             }
         },
-        Sawtooth => {
+        PeriodicFunction::Sawtooth => {
             for (i, s) in samples.iter_mut().enumerate() {
-                let p = i as f64 / sample_rate as f64; 
+                let p = i as f64 / sample_rate as f64;
                 *s = 2.0 * (freq/p - (0.5 + freq/p).floor());
             }
         },
-        Null => {
+        PeriodicFunction::Null => {
             for s in samples.iter_mut() {
                 *s = 0.0;
             }
